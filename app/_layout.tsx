7905214003cfd27a21272b0b62a9dcecc1caa140
JoaoWaselkin <joaorwl@gmail.com>
2025-04-29 18:11:01 -0300

    feature: retirei a headershown

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+// app/_layout.tsx
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -8,7 +9,7 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+// Previne o splash de sumir antes de tudo carregar
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -29,7 +30,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={DefaultTheme}>
-      <Stack>
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="(stacks)" />
       </Stack>
